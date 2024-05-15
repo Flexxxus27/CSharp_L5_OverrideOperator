491ff55d6ae5837e8cafc23e6078cdd9051011ef
user <user@user.ru>
2024-05-15 21:16:15 +0300

    Добавьте файлы проекта.

---
+++ Employee.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_L5
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string Phone {  get; set; }
+        public string Email { get; set; }
+        public string Position { get; set; }
+        public string Responsibilities { get; set; }
+        public decimal Salary { get; set; }
+        public Employee(string name, DateTime dateOfBirth, string phone, string email, string position, string responsibilities, decimal salary)
+        {
+            this.Name = name;
+            this.dateOfBirth = dateOfBirth;
+            this.Phone = phone;
+            this.Email = email;
+            this.Position = position;
+            this.Responsibilities = responsibilities;
+            this.Salary = salary;
+        }
+        public static decimal operator +(Employee a, decimal salary)
+        {
+            return a.Salary + salary;
+        }
+        public static decimal operator -(Employee a, decimal salary)
+        {
+            return a.Salary + salary;
+        }
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Salary == b.Salary;
+        }
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return a.Salary != b.Salary;
+        }
+        public static bool operator >(Employee a, Employee b)
+        {
+            return a.Salary > b.Salary;
+        }
+        public static bool operator <(Employee a, Employee b)
+        {
+            return a.Salary < b.Salary;
+        }
+        public override bool Equals(object? obj)
+        {
+            if(obj is Employee other)
+            {
+                return this.Salary == other.Salary;
+            }
+            return false;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Date: {dateOfBirth}, " +
+                $"Phone: {Phone}, Email: {Email}, position:" +
+                $"{Position}, responsibilites: {Responsibilities}" +
+                $"Salary: {Salary}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,138 @@
+﻿namespace CSharp_L5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee("Иванов Иван Иванович", new DateTime(1990, 5, 14), "+79342392", "Ivanov@ex.com", "Программист", "Разработка ПО", 50000);
+            Employee employee2 = new Employee("Альбертов Генадий Сергеевич", new DateTime(1985, 2, 23), "+79678322", "Albertov@ex.com", "Дизайнер", "3D дизайн", 47000);
+
+            Console.WriteLine(employee1.ToString());
+            Console.WriteLine(employee2.ToString());
+            employee2.Salary = employee2 + 3000;
+            Console.WriteLine(employee1.Equals(employee2));
+        }
+    }
+        public class A 
+        {
+            public int Abc { get; set; }
+            public float Bbc { get; set; }
+            public A(int Abc, float Bbc)
+            {
+                this.Abc = Abc;
+                this.Bbc = Bbc;
+            }
+
+            public static float operator +(A a, float b) { return a.Abc + b; }
+            public static int operator +(A a, A b) { return a.Abc + b.Abc; }
+            public static bool operator >(A a, A b)
+            {
+                if (a.Abc > b.Abc) return false;
+                else return true;
+            }
+            public static bool operator <(A a, A b)
+            {
+                if (a.Abc > b.Abc) return false;
+                else return true;
+            }
+            public static A operator ++(A a)
+            {
+                a.Abc++;
+                return a;
+            }
+            public static A operator --(A a)
+            {
+                a.Abc--;
+                return a;
+            }
+
+            public static A operator -(A a) { a.Abc = -a.Abc; return a; }
+            public static bool operator true(A a) {  return a.Abc > 0; }
+            public static bool operator false(A a) { return a.Abc == 0; }
+            public static A operator &(A a, A b) { return a; }
+            //public static implicit operator Point (A a) { } //неявное
+            //public static explicit operator Point(A a) { }  //явное
+    }
+
+        public class Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+            public Point() { }
+            public static Point operator &(Point a, Point b)
+            {
+                if((a.X != 0 && a.Y != 0) && (b.X != 0 && b.Y != 0))
+                {
+                    return b;
+                }
+                return new Point();
+            }
+            public static bool operator true(Point p)
+            {
+                return p.X != 0 && p.Y != 0 ? true : false;
+            }
+            public static bool operator false(Point p)
+             {
+                 return p.X == 0 && p.Y == 0 ? true : false;
+             }
+
+        }
+    abstract class Figure
+    {
+        public abstract void Draw();
+    }
+    abstract class Quadrangle : Figure { }
+
+    class Rectangle 
+    { 
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle(int Width, int Height) 
+        { 
+            this.Width = Width;
+            this.Height = Height;
+        }
+
+        public static implicit operator Rectangle(Square s)
+        {
+            return new Rectangle(s.Length * 2, s.Length);
+        }
+    }
+    class Square 
+    {
+        public int Length {  get; set; }
+        
+    }
+
+    public class Laptop
+    {
+        public string Vendor {  get; set; }
+        public double Price {  get; set; }
+
+        public override string ToString()
+        {
+            return $"{Vendor} {Price}";
+        }
+    }
+    public class Shop
+    {
+        Laptop[] laptopArr;
+        public Shop(int size)
+        {
+            laptopArr = new Laptop[size];
+        }
+        public Laptop this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < laptopArr.Length) { return laptopArr[index]; }
+                throw new IndexOutOfRangeException();
+            }
+            set
+            {
+                laptopArr[index] = value;
+            }
+        }
+    }
+
+}
